8bdd92236d24094237e689ec153106063f413ea6
rotschopf <rentafranz@gmail.com>
2013-01-24 19:27:05 +0100

    added features log to syslog and stdout

--- idle-spindown/debian/idle-spindown.cron.d
+++ idle-spindown/debian/idle-spindown.cron.d
@@ -3,8 +3,8 @@
 # M   H     D M DOW root idle-spindown sdX
 # check activity on sda every 5 minutes
 # max 10 minutes with spin and no activity
-# 5   *     * *  *  root /usr/sbin/idle-spindown sda
+# 5   *     * *  *  root /usr/sbin/idle-spindown -nl sda
 # check activity on harddrive with partition that has a46 in its UUID every 15 minutes
-# */15 *    * *  *  root /usr/sbin/idle-spindown a46eb29c
+# */15 *    * *  *  root /usr/sbin/idle-spindown -nl a46eb29c
 # check activity between worktime every hour
-# 0 8-17    * * 1,2,3,4,5 root /usr/sbin/idle-spindown sdb
+# 0 8-17    * * 1,2,3,4,5 root /usr/sbin/idle-spindown -nl sdb
